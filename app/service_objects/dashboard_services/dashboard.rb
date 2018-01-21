56435094b628e1d9f765cb3cacbf69392bc911c5
anoam <anoam@yandex.ru>
2018-01-21 14:43:36 +0300

    Dashboard service simplified

--- app/service_objects/dashboard_services/dashboard.rb
+++ app/service_objects/dashboard_services/dashboard.rb
@@ -2,20 +2,23 @@ module DashboardServices
   class Dashboard
 
     def total_boards
-      @total_boards ||= Board.all.count
+      @total_boards ||= all_boards.count
     end
 
     def total_tasks
-      @total_tasks ||= Board.all.inject(0) do |a, e|
-        a += e.tasks.count
-      end
+      @total_tasks ||= all_tasks.count
     end
 
     def total_incomplete_tasks
-      @total_incomplete_tasks ||= Board.all.inject(0) do |a, e|
-        a += e.tasks.where(completed_at: nil).count
-      end
+      @total_incomplete_tasks ||= all_tasks.incompleted.count
     end
 
+    def all_boards
+      Board
+    end
+
+    def all_tasks
+      Task
+    end
   end
 end
