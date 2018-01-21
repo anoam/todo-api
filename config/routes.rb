ff173d1da18eebd17597d557faafd846390e0b42
anoam <anoam@yandex.ru>
2018-01-21 21:40:03 +0300

    unused routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root 'dashboard#index'
 
   constraints format: :json do
-    resources :boards, shallow: true do
-      resources :tasks do
+    resources :boards, shallow: true, except: %i(new edit) do
+      resources :tasks, except: %i(new edit) do
         member do
           put :complete
         end
